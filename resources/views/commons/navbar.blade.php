4716a6d73bb52fc51565f85efeacba367efe820d
it <s073009.git@gmail.com>
2019-06-28 12:36:33 +0000

    top page

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,18 @@
+<header class="mb-4">
+    <nav class="navbar navbar-epand-sm navbar-dark bg-dark">
+        <a class="navbar-brand" href="/">Microposts</a>
+
+        <button tyoe="button" class="navbar-toggler" data-toggle=
+        "collapse" data-target="#navbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="navbar">
+            <ul class="navbar-nav mr-auto">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a href="#" class="nav-link">Signup</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
